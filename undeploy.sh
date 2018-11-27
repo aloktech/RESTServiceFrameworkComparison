e803ae83b6057c38d82340ee3473957fcbb8a2a0
aloktech <meher.ranjan@gmail.com>
2018-11-27 07:28:19 +0530

    deploy and undeploy script are added

---
+++ undeploy.sh
@@ -0,0 +1,13 @@
+clear
+docker stop ui
+docker stop ui.rest
+docker stop rest.jersey.payara
+docker stop rest.jersey.tomcat
+docker stop rest.resteasy.wildfly
+docker stop rest.jooby
+docker stop rest.javalin
+docker stop rest.spark
+docker stop rest.helidon
+docker stop rest.springboot
+docker stop rest.proteus
+docker ps -n 20
